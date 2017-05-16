871c8f8959fc125fd59658582e89d98380e3c9e6
Colley <pc1091@hallam.shu.ac.uk>
2017-05-16 12:07:21 +0100

    linting

--- src/RESD-CREATE-GUIDANCE/index.js
+++ src/RESD-CREATE-GUIDANCE/index.js
@@ -1,11 +1,8 @@
 import validation from './validation';
 
 function RESDInit() {
-
 	validation.initToggleCheck();
-
 	$('div[data-applylc]').on('change', validation.toggleCheck);
-
 }
 
 sits_attach_event('window','load',function() {// eslint-disable-line
