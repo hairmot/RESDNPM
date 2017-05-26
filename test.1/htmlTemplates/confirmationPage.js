f7dc0d5e5572634ff80ceb671d43b686dc378727
Colley <pc1091@hallam.shu.ac.uk>
2017-05-26 09:34:21 +0100

    removing directories

--- test.1/htmlTemplates/confirmationPage.js
+++
@@ -1,1 +0,0 @@
-export default `<div id="container"></div><input type="checkbox" data-accept />`;
--- test.1/htmlTemplates/lcRadioPickerNa.js
+++
@@ -1,6 +0,0 @@
-export default `
-                <div class="sv-col-md-4 sv-col-md-offset-4">	            
-                <div class=""><div class="sv-radio"><label for="ANSWER.TTQ.MENSYS.2.1"><input type="radio" name="ANSWER.TTQ.MENSYS.2" id="ANSWER.TTQ.MENSYS.2.1" value="Y" data-webvalidation="on" data-ttqseqn="51" class=""> Yes - extension due to learning contract</label></div><div class="sv-radio"><label for="ANSWER.TTQ.MENSYS.2.2"><input type="radio" name="ANSWER.TTQ.MENSYS.2" id="ANSWER.TTQ.MENSYS.2.2" value="N" data-webvalidation="on" data-ttqseqn="51" class=""> No - extension for another reason</label></div></div><input type="hidden" name="DUM_FIXT.TTQ.MENSYS.2">                           
-                </div>
-                <div class="sv-col-sm-2 "><input type="submit" title="Next" value="Next" name="NEXT.DUMMY.MENSYS.1" class="sv-btn sv-btn-block sv-btn-primary" disabled=""></div>
-              `;
