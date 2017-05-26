1dd9b1ae4e33d79c4405b686c2742a78e76d7725
Colley <pc1091@hallam.shu.ac.uk>
2017-05-26 09:17:04 +0100

    re-enabling summary tests

---
+++ test/summary/summary.noEvidenceMode.test.js
@@ -0,0 +1,46 @@
+var mocha = require('mocha');
+var expect = require('chai').expect;
+var jsdom = require("jsdom");
+var { JSDOM } = jsdom;
+import setGlobals from '../aasits_function_mocks';
+import evidenceMode from '../../src/RESD-CREATE-SUMMARY/evidenceMode';
+import stuview from '../htmlTemplates/summaryNoEvidence';
+
+describe('Summary Screen', function() {
+	var $;
+
+	it('Hides the uploader if the "no  evidence available" tick box is ticked', function() {
+		$('input[data-evidenceavailable]').prop('checked',true);
+		evidenceMode();
+		expect($('[id^="PLUP_uploader"]').css('display')).to.equal('none');
+	});
+
+	it('Show the uploader if the reverse is true', function() {
+		$('input[data-evidenceavailable]').prop('checked',false);
+		evidenceMode();
+		//wait for fade in
+		setTimeout(function() {
+		expect($('[id^="PLUP_uploader"]').css('display')).to.equal('block');
+		},1000);
+	});
+
+	it('Show the uploader if the reverse is true', function() {
+		$('input[data-evidenceavailable]').prop('checked',true);
+		$('input[data-evidencereason]').val('')
+		evidenceMode();
+		//wait for fade in
+		setTimeout(function() {
+		expect($('[id^="PLUP_uploader"]').css('display')).to.equal('none');
+		},1000);
+	});
+
+
+
+     before(() => {
+        var dom = new JSDOM(stuview);
+        $ = require('jquery')(dom.window);
+        global.$ = $;
+		setGlobals();
+    });
+
+});
