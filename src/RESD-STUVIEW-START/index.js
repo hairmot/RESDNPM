88d96e086f3b26448d625816e9e7ce99c34c5c82
Colley <pc1091@hallam.shu.ac.uk>
2017-05-31 11:50:46 +0100

    adding re-usable tour functionality

--- src/RESD-STUVIEW-START/index.js
+++ src/RESD-STUVIEW-START/index.js
@@ -1,10 +1,12 @@
 import eventHandlers from './eventHandlers';
 import '../shared/css/fancyLoadingButton.css';
+import tour from '../shared/js/tour';
 
 function RESDInit() {
 	eventHandlers.deleteButtonClicked();
+	tour.initTour();
 }
 
-sits_attach_event('window','load',function() { 
+sits_attach_event('window','load',function() {
 	RESDInit();
 });
