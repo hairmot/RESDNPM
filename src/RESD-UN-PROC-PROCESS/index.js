91f16151c5a2349c47ee8497b8fac50806de4650
hairmot <peter.colley@hotmail.com>
2017-12-22 10:25:10 +0000

    latest

--- src/RESD-UN-PROC-PROCESS/index.js
+++ src/RESD-UN-PROC-PROCESS/index.js
@@ -4,7 +4,7 @@ import '../shared/css/toastr.css';
 
 function RESDInit() {
 	eventHandlers.init();
-	validation.verifyPage();
+	validation.validatePage();
 }
 
 sits_attach_event('window', 'load',function() {
